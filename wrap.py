57bb549ad3365b06113fa976274bc7fb9e29986c
Alex Myasoedov <msoedov@gmail.com>
2017-04-13 22:07:10 -0400

    Run docker build if tag passed

--- wrap.py
+++ wrap.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from subprocess import check_output
+from subprocess import CalledProcessError, check_output
 
 import fire
 from pigar.reqs import file_import_modules, is_stdlib
@@ -24,9 +24,15 @@ CMD python -c "import os;b=os.getenv('PY_EXEC');b=b.replace('1l', '\\n');exec(b)
 
 """
 
+
 def read_from(path):
     with open(path, "r") as fp:
         return fp.read()
+
+
+def write_to(path, data):
+    with open(path, "w") as fp:
+        return fp.write(data)
 
 
 def maintainer():
@@ -34,13 +40,16 @@ def maintainer():
         name = check_output('git config user.name', shell=True).decode('utf-8').strip(' \n')
         email = check_output('git config user.email', shell=True).decode('utf-8').strip(' \n')
         return '{} <{}>'.format(name, email)
-    except:
+    except CalledProcessError:
         user = os.getenv('USER')
         return '{user} <{user}@localhost>'.format(user=user)
 
 
-def generate(module):
-
+def generate(module, tag=None):
+    """
+    Pack a python module into Dockerfile one liner
+    Build it if tag specified.
+    """
     source = read_from(module)
     modules = file_import_modules('', source)[0]
     req = [m for m, v in modules.items() if not is_stdlib(m)]
@@ -48,7 +57,14 @@ def generate(module):
     tokens = listified_tokenizer(source)
     compressed = minify(tokens, argparse.Namespace(tabs=False))
     compressed = compressed.replace('\"', '\'').replace('\n', '1l')
-    print(dockerfile.format(requirements=req, body=compressed, maintainer=maintainer()))
+    artifact = dockerfile.format(requirements=req, body=compressed, maintainer=maintainer())
+    if not tag:
+        print(artifact)
+    else:
+        docker_name = 'Docker.gen'
+        write_to(docker_name, artifact)
+        os.system('docker build -t {tag} -f {docker_name} .'.format(tag=tag, docker_name=docker_name))
+        os.remove(docker_name)
 
 
 fire.Fire(generate)
